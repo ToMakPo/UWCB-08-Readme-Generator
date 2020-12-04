47ea0b4a04b5875552c8ce53c86035e1a0df26aa
Makai Post <postmakai@gmail.com>
2020-12-03 23:43:48 -0800

    tested

--- assets/scripts/metadata.js
+++ assets/scripts/metadata.js
@@ -2,36 +2,39 @@ const fs = require('fs')
 
 const path = './assets/scripts/data.json'
 
-const Data = function({lastSavedPath, team}) {
-    let lastSavedPath = lastSavedPath
+const Data = function(data) {
+    let lastSavedPath = data.lastSavedPath
     this.getLastSavedPath = () => lastSavedPath
     this.setLastSavedPath = path => {
         lastSavedPath = path
         save()
     }
 
-    let team = team
+    let team = data.team
     this.getTeam = () => team
     this.addTeamMember = (name, image='', phone='', email='', ...profiles) => {
+        phone = phone.toString().replace(/[^0-9]/g, '')
+        console.log('profiles:', profiles);
         const member = { name, image, phone, email, profiles }
-        team.push(member)
+        team[name] = member
         save()
         return member
     }
-    this.newProfile = (text, url) => {text, url}
+    this.newProfile = (text, url) => { return {text, url} }
+    this.removeTeamMember = name => {
+        delete team[name]
+        save()
+    }
 
     const save = function() {
-        fs.writeFileSync(path, JSON.parse({
-            lastSavedPath: this.lastSavedPath,
-            team: this.team
-        }))
+        fs.writeFileSync(path, JSON.stringify({ lastSavedPath, team }))
     }
 }
 
-const importData = function(path) {
+const importData = function() {
     const raw = fs.existsSync(path) ? JSON.parse(fs.readFileSync(path, 'utf-8')) : {
         lastSavedPath: './',
-        team: []
+        team: {}
     }
     return new Data(raw)
 }
